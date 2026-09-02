eccc08313f254ba660fedb26a02d0931235175c3
Talha Tahir <140109354+Talha-Tahir2001@users.noreply.github.com>
2026-09-02 16:36:18 +0500

    feat: init api setup

---
+++ app/api/sessions/route.ts
@@ -0,0 +1,11 @@
+import { NextRequest } from "next/server";
+
+export async function GET(req: NextRequest) {
+    
+    return new Response(JSON.stringify({ message: "Hello from the sessions API!" }), {
+        status: 200,
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+}
